9c19eeb7ed622ad1ad75050c11ed7acce9f2553f
Boa Jang <boajang97@gmail.com>
2023-06-08 11:12:20 +0900

    update sample

--- data.js
+++
@@ -1,7 +0,0 @@
-var data = [
-    {pid: "ZIDJ03299045561045", oculus: "OS"},
-    {pid: "KSHC87032542106797", oculus: "OS"},
-    {pid: "JAWZ65545007516334", oculus: "OS"},
-    {pid: "HOCR97372566694488", oculus: "OD"},
-    {pid: "HOCR97372566694488", oculus: "OS"},
-];
