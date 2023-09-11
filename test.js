2081a6e7c46edb2d4db9aca170cc750075c127ef
Boa Jang <boajang97@gmail.com>
2023-09-11 16:29:10 +0900

    Update

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 var data = [
     {"pid": "46872852", "laterality": "2", "disease": "GON"}, 
     {"pid": "55100962", "laterality": "1", "disease": "GON"}
+    {"pid": "46872852", "laterality": "2", "disease": "Normal"}, 
+    {"pid": "55100962", "laterality": "1", "disease": "Normal"}
     ]
